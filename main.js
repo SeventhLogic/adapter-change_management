ba8c34ab297606e99be0b67af400db04b5c5ebb9
SeventhLogic <cyrusskolnick@gmail.com>
2020-05-14 19:01:41 +0000

    Lab 1

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // Update this constant with your ServiceNow credentials
 const options = {
-  url: 'yourDevServiceNowInstanceCanonicalHostname',
-  username: 'devUsername',
-  password: 'devUserPassword'
+  url: 'https://dev89121.service-now.com/',
+  username: 'admin',
+  password: '@Kairiken019'
 };
 
 
